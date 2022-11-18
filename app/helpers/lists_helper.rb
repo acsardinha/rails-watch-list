af4823174490ebce9ef7b4511caec2be8b7cdf7f
Andre Sardinha <acsardinha@hotmail.com>
2022-11-18 17:19:07 +0000

    Watch List ALMOST FINISHED HEROKU (NAOFAZFILHOS)

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,9 @@
 module ListsHelper
+  def cloudinary_image(list)
+    if list.photo.attached?
+      cl_image_path list.photo.key, crop: :fill
+    else
+      'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png'
+    end
+  end
 end
